40ea9e6ebb27d7ca8387641d2bd9f958cb60d3bc
sejod <sejodmh@gmail.com>
2024-11-17 14:52:22 +0200

    first commit

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    //  int a =20;
+    //  int b=50;
+    //  Debug.Log(a*b);P
+    
+
+        
+        
+
+    }
+
+
+
+
+
+
+     }
+
+
+        
+    
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
